a99c51ff8ceac6245e1f3b522853aa142dda47e0
STRLENgit <groupstroyresurs@mail.ru>
2019-11-22 17:02:31 +0300

    addFunctionSum

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,9 +1,13 @@
-/**
+/* *
  * Power
  * @param {number} m base
  * @param {number} n index
  * @returns {number}
  */
-function sum(m, n) {
 
+function sum(m, n) {
+    return m + n;
 }
+
+let result = sum(1, 1);
+alert( result ); 
