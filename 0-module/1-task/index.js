dc7a6ec156401371a1b9afe89d09fe2acdead2ce
Sergei <sergeiforit@gmail.com>
2019-12-03 12:30:29 +0300

    Fixed Warnings

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -4,6 +4,13 @@
  * @param {number} n index
  * @returns {number}
  */
-function sum(m, n) {
 
+function sum(m, n) {
+  return m + n;
 }
+
+/* let showFirstResult = sum(1, 1);
+alert(showFirstResult); 
+
+let showSecondResult = sum(2, 5);
+alert(showSecondResult);  */
--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,6 +4,21 @@
  * @param {number} n index
  * @returns {number}
  */
-function pow(m, n) {
 
+function pow(x, n) {
+  let showResult = x;
+
+  for (let i = 1; i < n; i++) {
+    showResult *= x;
+  }
+
+  return showResult;
+}
+let x = prompt('Введите целое число?', '');
+let n = prompt('Введите степень?', '');
+
+if (n < 1) {
+  alert('Степень ${n} некорректна. Введите целое число');
+} else {
+  alert(pow(x, n));
 }
