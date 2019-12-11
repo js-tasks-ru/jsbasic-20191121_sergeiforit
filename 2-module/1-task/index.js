829fec414e1c6db38498ab292bd52709e01d3821
Sergei <sergeiforit@gmail.com>
2019-12-11 15:43:49 +0300

    Function clone(obj)

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -9,7 +9,7 @@
 function clone (obj) {
   let copy = {}; // создаем новый пустой объект
   for (const key in obj) { // перебираем объект
-    if ( typeof (obj[key] ) === 'object' && (obj[key]) !== null) { // второе условие для корректной обработки typeof null === 'object'
+    if (typeof (obj[key]) === 'object' && (obj[key]) !== null) { // второе условие для корректной обработки typeof null === 'object'
       copy[key] = clone(obj[key]); // рекурсия для вложенных объектов
     } else {
       copy[key] = obj[key]; // копируем обычные ключ-значения объекта
