9056e3f4c2c17a6aa0e0df8d7c334ad0bea8fb7e
Sergei <sergeiforit@gmail.com>
2019-12-13 18:05:44 +0300

    function getMinMax(string)

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,4 +4,29 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+  let numbers = [];
+  let arr = str.split(',');
+    
+  if (arr.length > 3) {
+    for (let i = 0; i < arr.length; i++) {
+      if (!isNaN(+arr[i])) {
+        numbers.push(+arr[i]);
+      }
+    }
+  } else {    
+    arr = arr.join('').split(' '); 
+    for (let i = 0; i < arr.length; i++) {
+      if (!isNaN(+arr[i])) {
+        numbers.push(+arr[i]);
+      }
+    }
+  }
+
+  let minNumbers = Math.min(...numbers);
+  let maxNumbers = Math.max(...numbers);
+
+  return {
+    min: minNumbers,
+    max: maxNumbers
+  };
 }
