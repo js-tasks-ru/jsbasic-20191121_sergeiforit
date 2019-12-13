93e2044cd37d43decfc8f97de7116a71e2459693
Sergei <sergeiforit@gmail.com>
2019-12-13 17:50:27 +0300

    Function showSalary(data,age)

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -4,5 +4,16 @@
  * @param {number} age - максимальный возраст
  * @returns {string}
  */
+
 function showSalary(data, age) {
+  let result = '';
+  for (let i = 0; i < data.length; i += 1) {
+    for (let key in data[i]) {
+      if (data[i].age <= age) {
+        result += (`${data[i].name}, ${data[i].balance}\n`);
+      }
+      break;
+    }
+  }
+  return result.slice(0, result.length - 1);
 }
